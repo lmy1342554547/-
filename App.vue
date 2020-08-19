e717bb71a72f148753c0c35f0c51c274df423d88
李明扬 <1342554547@qq.com>
2020-08-19 21:37:21 +0800

    更新

--- App.vue
+++ App.vue
@@ -1,32 +1,6 @@
 <script>
-import Vue from "vue";
 export default {
-	onLaunch() {
-		uni.getSystemInfo({
-			success: res => {
-				// #ifndef MP
-				Vue.prototype.StatusBar = res.statusBarHeight;
-				if (res.platform == "android") {
-					Vue.prototype.CustomBar = res.statusBarHeight + 50;
-				} else {
-					Vue.prototype.CustomBar = res.statusBarHeight + 45;
-				}
-				// #endif
-
-				// #ifdef MP-WEIXIN
-				Vue.prototype.StatusBar = res.statusBarHeight;
-				let custom = wx.getMenuButtonBoundingClientRect();
-				Vue.prototype.Custom = custom;
-				Vue.prototype.CustomBar = custom.bottom + custom.top - res.statusBarHeight;
-				// #endif
-
-				// #ifdef MP-ALIPAY
-				Vue.prototype.StatusBar = res.statusBarHeight;
-				Vue.prototype.CustomBar = res.statusBarHeight + res.titleBarHeight;
-				// #endif
-			}
-		});
-	}
+	onLaunch() {}
 };
 </script>
 
